af3dd934ab4e0fc81f69a0bf6d9075b420673b8f
Manel Romero Garcia <manel.romero.garcia@gmail.com>
2017-11-27 19:43:24 +0100

    Terminado Jugador y Casilla

--- src/juego/Jugador.java
+++ src/juego/Jugador.java
@@ -10,5 +10,31 @@ package juego;
  * @author Manel
  */
 public class Jugador {
-    
+   public String ficha;
+   public String nombre;
+
+    public Jugador(String ficha, String nombre) {
+        this.ficha = ficha;
+        this.nombre = nombre;
+    }
+
+    public Jugador() {
+    }
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+   
 }
